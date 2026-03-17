27d65d6573cf98196c473b9aefcd9c10b1818b59
itsAbdullahMaqsood-work <abdullah@macromodule.net>
2026-03-17 08:57:09 +0500

    Implemented map

---
+++ lib/views/analytics_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AnalyticsView extends StatelessWidget {
+  const AnalyticsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Analytics',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
